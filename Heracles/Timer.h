d7a91dd2b1fc9ad4682eb82cc04c2d5b81a62fd6
Vivek Jain <viveksjain@gmail.com>
2012-12-31 22:42:05 +0800

    Add some more documentation to the Timer class

--- Heracles/Timer.h
+++ Heracles/Timer.h
@@ -1,6 +1,11 @@
 //
 //  Copyright (c) 2012 Vivek Jain. All rights reserved.
 //
+//  This class is a simple wrapper for NSTimer, with the additional properties
+//  that only one timer can be running at time, and the timer takes into account
+//  the time spent sleeping (whereas NSTimer considers any sleep time as 0
+//  seconds having elapsed).
+//
 
 #import <Foundation/Foundation.h>
 
